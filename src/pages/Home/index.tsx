cb1ff53aa39909080b549bfed59fe30b20c9a7f2
Leticia Rosae <leticiarosadeoliveira@yahoo.com.br>
2023-11-02 23:59:06 -0300

    Inicialized the page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,11 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 import Imagem from '../../assets/Imagem.png'
-import { ContainerHome, ContainerTextHome } from './styles'
+import {
+  ContainerElements,
+  ContainerHome,
+  ContainerIcon,
+  ContainerTextHome,
+} from './styles'
 export function Home() {
   return (
     <ContainerHome>
@@ -9,19 +15,33 @@ export function Home() {
           Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
           hora
         </h3>
+        <ContainerElements>
+          <div>
+            <ContainerIcon>
+              <ShoppingCart size={16} weight="fill" />
+            </ContainerIcon>
 
-        <div>
-          <h5>Compra simples e segura</h5>
-        </div>
-        <div>
-          <h5>Entrega rápida e rastreada</h5>
-        </div>
-        <div>
-          <h5>Embalagem mantém o café intacto</h5>
-        </div>
-        <div>
-          <h5>O café chega quentinho até você</h5>
-        </div>
+            <h5>Compra simples e segura</h5>
+          </div>
+          <div>
+            <ContainerIcon>
+              <Timer size={16} weight="fill" />
+            </ContainerIcon>
+            <h5>Entrega rápida e rastreada</h5>
+          </div>
+          <div>
+            <ContainerIcon>
+              <Package size={16} weight="fill" />
+            </ContainerIcon>
+            <h5>Embalagem mantém o café intacto</h5>
+          </div>
+          <div>
+            <ContainerIcon>
+              <Coffee size={16} weight="fill" />
+            </ContainerIcon>
+            <h5>O café chega quentinho até você</h5>
+          </div>
+        </ContainerElements>
       </ContainerTextHome>
       <img src={Imagem} alt="" />
     </ContainerHome>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,7 +4,7 @@ export const ContainerHome = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 3rem;
+  padding-top: 5.875rem;
   width: 100%;
   height: auto;
   img {
@@ -14,10 +14,77 @@ export const ContainerHome = styled.div`
   }
 `
 
+export const ContainerElements = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+  padding-top: 4.125rem;
+  div {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    :first-child {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: ${(props) => props.theme.cor.baseWhite};
+      border-radius: 100%;
+    }
+    h5 {
+      padding-left: 0.29rem;
+      font-size: ${(props) => props.theme.textSizes['text-regular-m']};
+      font-family: ${(props) => props.theme.fonts.regular};
+      font-weight: 100;
+    }
+  }
+  :first-child :first-child {
+    background-color: ${(props) => props.theme.cor.brandYellowDark};
+  }
+  :nth-child(2) :first-child {
+    background-color: ${(props) => props.theme.cor.brandYellow};
+  }
+  :nth-child(3) :first-child {
+    background-color: ${(props) => props.theme.cor.baseText};
+  }
+  :nth-child(4) :first-child {
+    background-color: ${(props) => props.theme.cor.brandPurple};
+  }
+`
+
+export const ContainerIcon = styled.div`
+  width: 32px;
+  height: 32px;
+`
+
 export const ContainerTextHome = styled.div`
   h1 {
     font-size: ${(props) => props.theme.textSizes['title-title-xl']};
     font-family: ${(props) => props.theme.fonts.title};
     font-weight: 800;
+  }
+  h3 {
+    padding-top: 1rem;
+    font-size: ${(props) => props.theme.textSizes['title-title-m']};
+    font-family: ${(props) => props.theme.fonts.regular};
+    font-weight: 100;
+  }
+`
+
+export const ContainerInfo = styled.div`
+  display: block;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :first-child {
+      color: ${(props) => props.theme.cor.baseWhite};
+      background-color: ${(props) => props.theme.cor.brandYellowDark};
+    }
   }
 `
